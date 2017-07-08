d2f990b94dfef7b1460f706aa1d374486d09cd23
Shaun Saker <info@shaunsaker.com>
2017-07-08 15:29:35 +0200

    Record and playback audio - basic

--- src/pages/VoiceNotes.js
+++ src/pages/VoiceNotes.js
@@ -2,10 +2,14 @@ import React from "react";
 import {
     View,
     Text,
+    TouchableOpacity,
 } from "react-native";
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-// import { AudioRecorder } from 'react-native-audio';
+import {
+    Player,
+    Recorder
+} from 'react-native-audio-toolkit';
 
 import utilities from '../utilities';
 import styleConstants from '../styles/styleConstants';
@@ -20,7 +24,13 @@ export class VoiceNotes extends React.Component {
     constructor(props) {
         super(props);
 
-        this.recordAudio = this.recordAudio.bind(this);
+        this.reloadRecorder = this.reloadRecorder.bind(this);
+        this.toggleRecording = this.toggleRecording.bind(this);
+        this.togglePlayback = this.togglePlayback.bind(this);
+
+        this.filePath = 'IdeaMe-' + utilities.createUID() + '.mp4';
+        this.recorder;
+        this.player;
 
         this.state = {
             voiceNotes: [],
@@ -34,10 +44,65 @@ export class VoiceNotes extends React.Component {
         };
     }
 
-    recordAudio() {
-        const path = '/data/user/0/com.ideaMeApp/files';
+    componentDidMount() {
+        this.reloadRecorder();
+        this.reloadPlayer();
+    }
 
+    reloadPlayer() {
+        if (this.player) {
+            this.player.destroy();
+        }
 
+        this.player = new Player(
+            this.filePath, 
+            {
+                autoDestroy: false
+            }
+        );
+    }
+
+    reloadRecorder() {
+        if (this.recorder) {
+            this.recorder.destroy();
+        }
+
+        this.recorder = new Recorder(
+            this.filePath,
+            {
+                bitrate: 256000,
+                channels: 2,
+                sampleRate: 44100,
+                quality: 'max',
+                //format: 'ac3', // autodetected from path
+                //encoder: 'aac', // autodetected from path
+            }
+        );
+    }
+
+    togglePlayback() {
+        this.player.playPause((error, playing) => {
+            console.log('Playback toggling');
+            if (error) {
+                console.log(error);
+            }
+            if (playing) {
+                console.log('Playing')
+            }
+        });
+    }
+
+    toggleRecording() {
+        this.recorder.toggleRecord((error, stopped) => {
+            console.log('Recording toggling');
+            if (error) {
+                console.log(error);
+            }
+            if (stopped) {
+                console.log('Recording stopped');
+                this.reloadRecorder();
+            }
+        });
     }
 
     render() {
@@ -57,6 +122,18 @@ export class VoiceNotes extends React.Component {
                     idea={this.props.idea}
                     handleAdd={this.recordAudio}
                     voiceNotes={this.state.voiceNotes} />
+
+                <TouchableOpacity
+                    onPress={this.toggleRecording}
+                    style={{ paddingVertical: 16 }}>
+                    <Text>Record</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={this.togglePlayback}
+                    style={{ paddingVertical: 16 }}>
+                    <Text>Play</Text>
+                </TouchableOpacity>
 
                 <SnackBar />
 
