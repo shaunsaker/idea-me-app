18cd1c8f2dcc0d81cea1e98ab85bff6285fbfda1
Shaun Saker <info@shaunsaker.com>
2017-07-19 17:59:47 +0200

    Update quotes

--- src/resources/quotes.js
+++ src/resources/quotes.js
@@ -4,10 +4,6 @@ const quotes = [
         author: 'Marie Curie'
     },
     {
-        title: 'No army can withstand the strength of an idea whose time has come.',
-        author: 'Victor Hugo'
-    },
-    {
         title: 'If you want something new, you have to stop doing something old.',
         author: 'Peter F. Drucker'
     },
@@ -24,21 +20,45 @@ const quotes = [
         author: 'Earl Nightengale'
     },
     {
-        title: 'An idea is salvation by imagination.',
-        author: 'Frank L. Wright'
-    },
-    {
         title: 'No matter what people tell you, words and ideas can change the world.',
         author: 'Robin Williams'
-    },
-    {
-        title: 'No army can withstand the strength of an idea whose time has come.',
-        author: 'Victor Hugo'
     },
     {
         title: 'Great minds discuss ideas. Average minds discuss events. Small minds discuss people.',
         author: 'Charles Stewart'
     },
+    {
+        title: 'Human history is, in essence, a history of ideas.',
+        author: 'Herbert G. Wells'
+    },
+    {
+        title: 'It is a lesson which all history teaches wise men, to put trust in ideas, and not in circumstances.',
+        author: 'Ralph Waldo Emerson'
+    },
+    {
+        title: 'A mind that is stretched to a new idea never returns to its original dimension.',
+        author: 'Oliver Wendell Holmes, Sr.'
+    },
+    {
+        title: 'Everyone is a genius at least once a year. The real geniuses simply have their bright ideas closer together.',
+        author: 'Georg Christoph Lichtenberg'
+    }, 
+    {
+        title: 'Lack of money is no obstacle. Lack of an idea is an obstacle.',
+        author: 'Ken Hakuta'
+    }, 
+    {
+        title: 'My ideas usually come not at my desk writing but in the midst of living.',
+        author: 'Anais Nin'
+    },
+    {
+        title: 'Capital isn’t that important in business. Experience isn’t that important. You can get both of these things. What is important is ideas.',
+        author: 'Harvey S. Firestone'
+    }, 
+    {
+        title: 'If I have a thousand ideas and only one turns out to be good, I am satisfied.',
+        author: 'Alfred Bernhard Nobel'
+    }, 
 ];
 
 export default quotes;
